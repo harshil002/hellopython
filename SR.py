904d73e31620262f2306f98a56267f79ada25d49
Harshil Chaudhary <34757900+harshil002@users.noreply.github.com>
2018-08-16 15:41:39 -0400

    Add files via upload

---
+++ SR.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[19]:
+
+#!pip install SpeechRecognition
+
+
+# In[24]:
+
+get_ipython().system('pip install pyaudio')
+
+
+# In[33]:
+
+get_ipython().system('pip install google-api-python-client')
+
+
+# In[2]:
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('Speak Please: ')
+    audio = r.listen(source)
+    
+    try:
+        text = r.recognize_google(audio)
+        print('You said: {}'.format(text))
+    except:
+        print("Sorry")
+
